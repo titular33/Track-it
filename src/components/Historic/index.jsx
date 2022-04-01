f4cc79f8c2cce68745f798b884649b995791c1c6
titular33 <amorim.pedro@engenharia.ufjf.br>
2022-04-01 18:14:30 -0300

    Fazendo o historic js, além de colocar os requisitos do css

--- src/components/Historic/index.jsx
+++ src/components/Historic/index.jsx
@@ -1,49 +1,124 @@
-import styled from 'styled-components';
-import HabitsCheck from './HabitsCheck';
+import Header from "../Header";
+import styled from "styled-components";
+import Footer from "../Footer";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import dayjs from "dayjs";
+import { useState, useEffect, useContext } from "react";
+import UserContext from "../../contexts/UserContexts";
+import axios from "axios";
+import CalendarClick from "./CalendarClick";
 
 
-export default function CalendarClick ({ state, setstate, information }){
-    console.log(information)
-    return(
-        <DayClick state = {state}>
-            <div>
-                <h2>Hábitos do dia {information[0]}</h2>
-                {information.length === 0? "" : information[1].map((n,i) => <HabitsCheck habitName = {n.name} done = {n.done}/>)}
-                <Close onClick={()=>setstate(!state)}>Fechar</Close>
-            </div>            
-        </DayClick >
-    )
-}
-const DayClick = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: red;
-    display: ${props => props.state? "blocked" : "none"};
-    position: fixed;
-    background-color: rgba(255,255,255,0.7);
-    z-index: 0;
-    div{
-        width: 90%;
-        margin: auto;
-        margin-top: 100px;
-        padding: 13px 13px 17px 15px;
-        background-color: #fff;
-        border-radius: 5px;
+
+export default function Historic() {
+    const [date, setDate] = useState(new Date());
+    const { user } = useContext(UserContext);
+    const [items, setItems] = useState([]);
+    const now = dayjs().format("DD/MM/YYYY");
+    const days = items.map((d, i) => d.day);
+    const [state, setstate] = useState(false);
+    const [information, setInformation] = useState([]);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
+  
+    useEffect(() => {
+      const request = axios.get(
+          `${process.env.REACT_APP_API_BASE_URL}/habits/history/daily`,
+        config
+      );
+  
+      request.then((resposta) => {
+        setItems(resposta.data);
+      });
+  
+      request.catch((error) => alert("Erro! Tente novamente :/"));
+    }, []);
+  
+    return (
+      <>
+        <Header />
+        <Habit>
+          <h2>Histórico</h2>
+        </Habit>
+        <Calendar
+          onChange={setDate}
+          value={date}
+          className="calendar"
+          locale="pt-br"
+          calendarType="US"
+          onClickDay={(value) => {
+            clickWeekday(days, value, setstate, items, setInformation);
+          }}
+          tileClassName={({ date }) => markedDays(days, date, items)}
+        />
+        <CalendarClick
+          state={state}
+          setstate={setstate}
+          information={information}
+        />
+        <Div />
+        <Footer />
+      </>
+    );
+    function clickWeekday(days, value, setState, items, setInfomation) {
+      if (days.find((x) => x === dayjs(value).format("DD/MM/YYYY"))) {
+        setState(true);
+        let chosen = [];
+        chosen = items.find(
+          (item, i) => item.day === dayjs(value).format("DD/MM/YYYY")
+        );
+        let habitChosen = [];
+  
+        for (let i = 0; i < chosen.habits.length; i++) {
+          habitChosen.push({
+            name: chosen.habits[i].name,
+            done: chosen.habits[i].done,
+          });
+        }
+  
+        setInfomation([dayjs(value).format("DD/MM/YYYY"), habitChosen]);
+      }
     }
-    h2{
-        font-family: 'Lexend Deca', sans-serif;
-        font-size: 23px;
-        color: #126BA5;
-        margin-bottom: 10px;
+  
+    function markedDays(days, date, items) {
+      let habit = [];
+      let doneHabit = [];
+  
+      if (days.find((x) => x === dayjs(date).format("DD/MM/YYYY"))) {
+        if (dayjs(date).format("DD/MM/YYYY") === now) {
+          return "today";
+        } else {
+          for (let i = 0; i < items.length; i++) {
+            if (items[i].day === dayjs(date).format("DD/MM/YYYY"))
+              habit = items[i].habits;
+            for (let j = 0; j < habit.length; j++) {
+              doneHabit.push(habit[j].done);
+            }
+          }
+          return `${doneHabit.reduce((acc, item) => acc && item, true)}`;
+        }
+      }
     }
-`
-const Close = styled.div`
-    font-family: 'Lexend Deca', sans-serif;
-    font-size: 16px;
-    color: #52B6FF;
-    text-align: center;
-    margin-top: 10px!important;
-`
+  }
+  
+  const Habit = styled.div`
+    margin: auto;
+    margin-top: 100px;
+    width: 90%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    h2 {
+      font-family: "Lexend Deca", sans-serif;
+      font-size: 23px;
+      color: #126ba5;
+    }
+  `;
+  
+  const Div = styled.div`
+    height: 110px;
+  `;
