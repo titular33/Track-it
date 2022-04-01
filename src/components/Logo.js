4c3f397d50fbbf68a3d0f1ba19c4013876d34d10
titular33 <amorim.pedro@engenharia.ufjf.br>
2022-04-01 01:43:29 -0300

    Corrigindo o Register e fazendo a tela do Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,21 @@
+import Image from '../images/logo.png';
+import styled from 'styled-components';
+
+export default function Logo() {
+    return(
+        <LogoImage>
+            <img src={Image}/> 
+        </LogoImage>
+    )
+}
+
+const LogoImage = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 10px;
+    margin-top: 68px;
+    img{
+        width: 180px;
+        height: 180px;
+    }
+`;
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,107 +1,117 @@
-import { useState } from 'react';
 import styled from 'styled-components';
-import { Link, useHistory} from 'react-router-dom';
-import axios from 'axios';
+import { Link, useHistory } from 'react-router-dom';
 import Loader from "react-loader-spinner";
-import Image from '../../images/logo.png';
+import Logo from '../Logo'
+import UserContext from '../../contexts/UserContexts';
+import axios from 'axios';
+import { useState, useEffect, useContext} from 'react';
 
-export default function Register(){
-
+ export default function Home (){
     const [email,setEmail] = useState ("");
     const [password, setPassword] = useState ("");
-    const [name, setName] = useState ("");
-    const [image, setImage] = useState ("");
     const [loading, setLoading] = useState(false);
-    let history = useHistory();
-
-    const body= {
+    const { setUser }= useContext(UserContext);
+    const body = {
         email,
-        name,
-        image,
         password
     }
+    let history = useHistory();
 
-    return(
+    useEffect(() =>{
+        if(localStorage.getItem('user')){
+            setUser(JSON.parse(localStorage.getItem('user')))
+            history.push("/hoje");
+        }}, [])
+
+    return (
         <>
-        <DivLogo>
-        <Logo src={Image}/> 
-     </DivLogo>
-     <Data>
-         <Input disabled = {loading} type="text" placeholder="email" onChange={e => setEmail(e.target.value)}></Input>
-         <Input disabled = {loading} type="password" placeholder="senha" onChange={e => setPassword(e.target.value)}></Input>
-         <Input disabled = {loading} type="text" placeholder="nome" onChange={e => setName(e.target.value)}></Input>
-         <Input disabled = {loading} type="text" placeholder="foto" onChange={e => setImage(e.target.value)}></Input>
-         <Button disabled = {loading} onClick={() => {
-                const request = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up", body);
-                request.then(response => history.push("/"));
-                request.catch(error => {
-                    alert("Erro! Preencha novamente os campos.")
-                    setLoading(false);
-                })
-                }}>
-                    {loading === true? "": "Cadastrar"}
-                    <Loader visible ={loading} type="ThreeDots" color="#FFF" height={80} width={80} /> 
-            </Button>
-        </Data>
-        <Link to="/">
-            <Login>Já tem uma conta? Faça login!</Login>
-        </Link>
+            <Logo />
+
+            <Data>
+                <input 
+                    disabled = {loading} 
+                    required 
+                    type="email" 
+                    placeholder="email" 
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <input 
+                    disabled = {loading} 
+                    required 
+                    type="password" 
+                    placeholder="senha" 
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <div disabled = {loading} onClick={() => login (setLoading, body, setUser)}>
+                        {loading === true? "": "Entrar"}
+                        <Loader 
+                            visible ={loading} 
+                            type="ThreeDots" 
+                            color="#FFF" 
+                            height={80} 
+                            width={80} 
+                        />
+                </div>
+            </Data>
+
+            <Link to="/cadastro">
+                <Register>Não tem uma conta? Cadastre-se!</Register>
+            </Link>
         </>
     )
-}
 
-const DivLogo = styled.div`
-    display: flex;
-    justify-content: center;
-    margin-bottom: 10px;
-    margin-top: 68px;
-`;
-
-const Logo = styled.img`
-    width: 180px;
-    height: 180px;
-    `;
-
-const Input = styled.input`
-    height: 45px;
-    font-family: 'Lexend Deca', sans-serif;
-    font-size: 20px;
-    color: #AFAFAF;
-    border: 1px solid #D5D5D5;
-    border-radius: 5px;
-    margin-bottom: 6px;
-    ::-webkit-input-placeholder  { 
-        color: #DBDBDB; 
-        padding: 11px;
+	function login (setLoading, body, setUser){
+        const request = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login", body);
+        setLoading(true);
+    
+        request.then(response => {
+            const user = JSON.stringify(response.data)
+            localStorage.setItem('user', user);
+            setUser(JSON.parse(localStorage.getItem('user')));
+            history.push("/hoje")});
+    
+        request.catch(error => {
+            alert("Erro! Email e/ou senha incorreto(s)");
+            setLoading(false);
+        })
     }
-`;
-
+}
 
 const Data = styled.div`
     display: flex;
     flex-direction: column;
     padding: 25px 36px;
+    input{
+        height: 45px;
+        font-family: 'Lexend Deca', sans-serif;
+        font-size: 20px;
+        color: #AFAFAF;
+        border: 1px solid #D5D5D5;
+        border-radius: 5px;
+        margin-bottom: 6px;
+            ::-webkit-input-placeholder  { 
+                color: #DBDBDB; 
+                padding: 11px;
+            }
+    }
+    div{
+        cursor: pointer;
+        height: 45px;
+        font-size: 21px;
+        font-family: 'Lexend Deca', sans-serif;
+        color: #fff;
+        background-color: #52B6FF;
+        border-radius: 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 `;
-
-const Button = styled.div`
-    cursor: pointer;
-    height: 45px;
-    font-size: 21px;
-    font-family: 'Lexend Deca', sans-serif;
-    color: #fff;
-    background-color: #52B6FF;
-    border-radius: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
-
-const Login = styled.p`
+const Register = styled.p`
     cursor: pointer;
     font-family: 'Lexend Deca', sans-serif;
     font-size: 14px;
     color: #52B6FF;
     text-decoration-line: underline;
     text-align: center;
-    margin-bottom: 30px;
-`
+`;
